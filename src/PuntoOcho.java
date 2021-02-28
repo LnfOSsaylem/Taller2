74b076a9c5e3246d4257c87423cabb8e78b40ede
Andrés Galván <62343874+afgalvan@users.noreply.github.com>
2021-02-27 23:33:50 -0500

    Corregir error de String multilinea con el "+"

--- src/PuntoOcho.java
+++ src/PuntoOcho.java
@@ -5,7 +5,7 @@ Para ello, se solicita utilizar solo una instrucción de impresión.
 public class PuntoOcho {
     public static void main(String[] args) {
         System.out.println("\033\143");
-        System.out.println("Menu de Opciones\nSeleccionar a continuacion una opcion: \n\n- Opcion 1
-        \n\n- Opcion 2\n\n- Opcion 3\n\n- Opcion 4\n\n- Opcion 5");
+        System.out.println("Menu de Opciones\nSeleccionar a continuacion una opcion: \n\n- Opcion 1"
+        + "\n\n- Opcion 2\n\n- Opcion 3\n\n- Opcion 4\n\n- Opcion 5");
     }
 }
